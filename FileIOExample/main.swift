7c8625c9e58e39a2d284676af2711dda6d13c4dc
yanghojoon <yanghojoon8487@gmail.com>
2022-01-10 20:39:17 +0900

    feat: FileIOExample 서브 디렉토리 생성

--- FileIOExample/main.swift
+++ FileIOExample/main.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
-let fileManager = FileManager.default
+let fileManager = FileManager.default // default는 fileManager의 싱글톤 인스턴스를 생성해줌
 
 let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first! // 왜 first를 붙이는 것일까??
 let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
 let tempURL = fileManager.temporaryDirectory // 이건 시스템에 위치한 tmp 폴더임. NSTemporaryDirectory와 동일
 
-
+// create subdirectory
+let appSupportDirectoryURL = appSupportURL.appendingPathComponent("FileIOExample")
+let documentsDirectoryURL = documentsURL.appendingPathComponent("FileIOExample")
+let tempDirectoryURL = tempURL.appendingPathComponent("FileIOExample")
+try? fileManager.createDirectory(at: appSupportDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+try? fileManager.createDirectory(at: documentsDirectoryURL, withIntermediateDirectories: true, attributes: nil)
+try? fileManager.createDirectory(at: tempDirectoryURL, withIntermediateDirectories: true, attributes: nil)
