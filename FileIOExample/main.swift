690d79b2e7e7ba2f69b6d3a59f09365264e79203
yanghojoon <yanghojoon8487@gmail.com>
2022-01-10 14:15:04 +0900

    feat: 파일매니저를 통해 파일을 관리할 디렉토리 URL 설정
    - ApplicationSupport
    - Documents
    - tmp

--- FileIOExample/main.swift
+++ FileIOExample/main.swift
@@ -7,5 +7,10 @@
 
 import Foundation
 
-print("Hello, World!")
+let fileManager = FileManager.default
+
+let appSupportURL = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first! // 왜 first를 붙이는 것일까??
+let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+let tempURL = fileManager.temporaryDirectory // 이건 시스템에 위치한 tmp 폴더임. NSTemporaryDirectory와 동일
+
 
